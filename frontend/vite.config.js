c1f79144f085dbf541b4d4fca54f0fe5a82c4d8f
SaiEswar002 <edapalapatisaieswar2005@gmail.com>
2025-05-11 06:23:46 +0530

    .

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // or '/' depending on your routing and folder structure
+  base: '/', // or '/' depending on your routing and folder structure
 })
